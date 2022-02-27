b357c023e04db641134e7870adb7150e4569985d
Jakub Sydor <sydorjakub@gmail.com>
2022-02-27 13:19:05 +0100

    feat: import modules to crypto-api

--- apps/crypto-api/src/app/app.module.ts
+++ apps/crypto-api/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { AppConfigModule } from '@nest-demo/config';
+import { AppJwtModule } from '@nest-demo/jwt';
+
+import { AuthModule } from '@nest-demo/auth';
+import { UserModule } from '@nest-demo/user';
+import { KeysModule } from '@nest-demo/keys';
+import { EncryptModule } from '@nest-demo/encrypt';
+
+const CONFIG_MODULES = [AppConfigModule, AppJwtModule];
+const DOMAIN_MODULES = [AuthModule, UserModule, KeysModule, EncryptModule];
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [...CONFIG_MODULES, ...DOMAIN_MODULES],
 })
 export class AppModule {}
--- apps/crypto-api/src/main.ts
+++ apps/crypto-api/src/main.ts
@@ -1,13 +1,29 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppConfigService } from '@nest-demo/config';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(AppConfigService);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto API - Demo')
+    .setDescription('Nest cryptography API demo')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
+
+  const port = configService.APP_PORT;
+  await app.init();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
